745e281fc487250f8ddaa7572786a50a43546d51
Code Sandbox <hello@codesandbox.io>
2022-05-24 14:37:17 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.41f26a62375b46b7144ebc544c995c48.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "43c44861f58a7b8cc04249e83b1a274c",
+    "url": "https://surendramg.github.io/csb-ydhd0g/index.html"
+  },
+  {
+    "revision": "88ade3cc5b3c08498c97",
+    "url": "https://surendramg.github.io/csb-ydhd0g/static/css/2.0908cace.chunk.css"
+  },
+  {
+    "revision": "21a9b1a76fd439b41262",
+    "url": "https://surendramg.github.io/csb-ydhd0g/static/css/main.0aacff0b.chunk.css"
+  },
+  {
+    "revision": "88ade3cc5b3c08498c97",
+    "url": "https://surendramg.github.io/csb-ydhd0g/static/js/2.3a93e236.chunk.js"
+  },
+  {
+    "revision": "21a9b1a76fd439b41262",
+    "url": "https://surendramg.github.io/csb-ydhd0g/static/js/main.b60eeb2f.chunk.js"
+  },
+  {
+    "revision": "c09bb0a672ab16fb2d0c",
+    "url": "https://surendramg.github.io/csb-ydhd0g/static/js/runtime~main.e407d8a9.js"
+  },
+  {
+    "revision": "c0906496a1e675e2e12c03446b89e1ce",
+    "url": "https://surendramg.github.io/csb-ydhd0g/static/media/annie-spratt-mcrQoYT8VFE-unsplash.c0906496.jpg"
+  },
+  {
+    "revision": "a0f933870918e152ccc05854dc113122",
+    "url": "https://surendramg.github.io/csb-ydhd0g/static/media/fusion-medical-animation-rnr8D3FNUNY-unsplash.a0f93387.jpg"
+  },
+  {
+    "revision": "f76702741cd4acd3c8e9c2088578b351",
+    "url": "https://surendramg.github.io/csb-ydhd0g/static/media/undraw_doctor_kw5l.f7670274.svg"
+  },
+  {
+    "revision": "5ab8a4deb2f8f6121fed9c7ae424f2e6",
+    "url": "https://surendramg.github.io/csb-ydhd0g/static/media/undraw_social_distancing_2g0u.5ab8a4de.svg"
+  },
+  {
+    "revision": "f313304f4b9ef7c36f6c26f99cfe93d2",
+    "url": "https://surendramg.github.io/csb-ydhd0g/static/media/undraw_throw_away_ldjd.f313304f.svg"
+  },
+  {
+    "revision": "f37fc53c397f57e868254efec17a74eb",
+    "url": "https://surendramg.github.io/csb-ydhd0g/static/media/undraw_wash_hands_nwl2.f37fc53c.svg"
+  }
+]);
